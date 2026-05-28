e550880c93a2e424d87ba9832f0a866bd09e3e3c
LeNguyenGiaBaoFITHCMUS <22120023@student.hcmus.edu.vn>
2026-05-28 17:08:46 +0700

    Update svg in README and update llm

--- rag_engine/llm.py
+++ rag_engine/llm.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_llm():
-    # Khởi tạo mô hình Gemini 1.5 Flash từ Google
+    # Khởi tạo mô hình Gemini 2.5 Flash từ Google
     return ChatGoogleGenerativeAI(
         model="gemini-2.5-flash-lite",
         temperature=0.1, # Temperature thấp = AI trả lời nghiêm túc, bám sát facts, không bay bổng
